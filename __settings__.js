2490f67f3d2140ea52829f27db121f1b6b10dd46
dev68i <166891052+dev68i@users.noreply.github.com>
2024-04-14 04:18:53 +0300

    Init

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "scene.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 174439022, 174439024, 174439085, 174467855, 174467861, 174467862 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/174439024/1/ammo.js', 'wasmUrl' : 'files/assets/174439023/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/174439022/1/ammo.wasm.js', 'preload' : true},
+];
